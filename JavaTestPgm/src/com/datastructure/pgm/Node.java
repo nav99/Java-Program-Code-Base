21b827ed56803d11d46def6bd5b527a04f8bdf64
nav99 <navneetgaur04@gmail.com>
2017-12-15 13:02:20 +0530

    add data strucure pgm

---
+++ JavaTestPgm/src/com/datastructure/pgm/Node.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datastructure.pgm;
+
+/**
+ *
+ * @author Mahadev
+ */
+public class Node {
+    int data;
+    Node next;
+    Node(int d)
+    {
+        this.data=d;
+        this.next=null;
+    }
+    
+}
